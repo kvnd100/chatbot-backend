01973c3064793fb2360277e25a73a6b30f3c304a
Kavindu <kvndranasinghe@gmail.com>
2026-04-13 10:18:43 +0530

    feat: enable TypeORM synchronize for development schema sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -22,6 +22,7 @@ import { AuthModule } from './auth/auth.module';
         password: configService.get('DB_PASSWORD'),
         database: configService.get('DB_NAME'),
         autoLoadEntities: true,
+        synchronize: true,
       }),
     }),
     ChatModule,
